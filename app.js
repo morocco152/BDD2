76d6281a380c440833429af0d1f906db385316b1
Luis Moroco <lmoroco@unsa.edu.pe>
2021-08-12 00:59:56 -0500

    diseños terminados

--- app.js
+++ app.js
@@ -5,24 +5,24 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
 
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index'); 
+const usersRouter = require('./routes/users'); 
 
 const app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname,'views')); 
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(cookieParser()); 
 app.use(express.static(path.join(__dirname, 'public')));
 
 // las rutas
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/users', usersRouter); 
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,49 @@
 const express = require('express');
 const router = express.Router();
 
-const ClienteController = require('../controllers/clientes.controller');
+const ClienteController = require('../controllers/clientes.controller'); // -.
+
 
 /* GET home page. */
 router.get('/ventas', function(req, res, next) {
   res.render('venta');
 });
-router.get('/productos', function (req, res){
-  res.render('venta');
+
+router.get('/dashboard', function (req, res){ //productos
+  res.render('dashboard');
 });
+
+router.get('/vales', function (req, res){
+  res.render('vales');
+});
+
+router.get('/productos', function (req, res){
+  res.render('productos');
+});
+
+router.get('/facturas', function (req, res){
+  res.render('facturas');
+});
+
+router.get('/gasto', function (req, res){
+  res.render('gastos');
+});
+
+router.get('/reserva', function (req, res){
+  res.render('reserva');
+});
+
+
+
+
+//index 
+router.get('/', function(req, res, next) {
+  res.send('indexm');
+});
+
+
+
+
 
 router.get('/clientes', ClienteController.formularioCrearCliente);
 router.post('/clientes', ClienteController.crearCliente);
